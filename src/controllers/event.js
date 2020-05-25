b8f031dca025a8c950dfc55a7383bef80af9a096
pepsiftd <pepsiftd@gmail.com>
2020-05-25 11:26:56 +0300

    Убирает именованные обработчики событий

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -36,47 +36,41 @@ export default class EventController {
   }
 
   render(event, offersData, destinations) {
-    const rollupButtonClickHandler = () => {
-      this.replaceDefaultWithEdit();
-    };
-
-    const editFormSubmitHandler = (evt) => {
-      evt.preventDefault();
-      this.replaceEditWithDefault();
-    };
-
-    const favoriteClickHandler = () => {
-      this._onDataChange(this, event, Object.assign({}, event, {
-        isFavorite: !event.isFavorite
-      }));
-    };
-
-    const eventTypeChangeHandler = (evt) => {
-      this._onDataChange(this, event, Object.assign({}, event, {
-        type: evt.target.value,
-        offers: offersData.transfer.get(evt.target.value) ? offersData.transfer.get(evt.target.value) : offersData.activity.get(evt.target.value),
-      }));
-    };
-
-    const destinationChangeHandler = (evt) => {
-      this._onDataChange(this, event, Object.assign({}, event, {
-        destination: evt.target.value,
-        description: destinations.find((it) => it.name === evt.target.value).description,
-        photos: destinations.find((it) => it.name === evt.target.value).photos,
-      }));
-    };
-
     const oldEventComponent = this._eventComponent;
     const oldEditEventComponent = this._editEventComponent;
 
     this._eventComponent = new EventComponent(event, offersData);
     this._editEventComponent = new EditEventComponent(event, offersData, destinations);
 
-    this._eventComponent.setRollupButtonClickHandler(rollupButtonClickHandler);
-    this._editEventComponent.setSubmitHandler(editFormSubmitHandler);
-    this._editEventComponent.setFavoriteClickHandler(favoriteClickHandler);
-    this._editEventComponent.setEventTypeChangeHandler(eventTypeChangeHandler);
-    this._editEventComponent.setDestinationChangeHandler(destinationChangeHandler);
+    this._eventComponent.setRollupButtonClickHandler(() => {
+      this.replaceDefaultWithEdit();
+    });
+
+    this._editEventComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      this.replaceEditWithDefault();
+    });
+
+    this._editEventComponent.setFavoriteClickHandler(() => {
+      this._onDataChange(this, event, Object.assign({}, event, {
+        isFavorite: !event.isFavorite
+      }));
+    });
+
+    this._editEventComponent.setEventTypeChangeHandler((evt) => {
+      this._onDataChange(this, event, Object.assign({}, event, {
+        type: evt.target.value,
+        offers: offersData.transfer.get(evt.target.value) ? offersData.transfer.get(evt.target.value) : offersData.activity.get(evt.target.value),
+      }));
+    });
+
+    this._editEventComponent.setDestinationChangeHandler((evt) => {
+      this._onDataChange(this, event, Object.assign({}, event, {
+        destination: evt.target.value,
+        description: destinations.find((it) => it.name === evt.target.value).description,
+        photos: destinations.find((it) => it.name === evt.target.value).photos,
+      }));
+    });
 
     if (oldEventComponent && oldEditEventComponent) {
       replace(this._eventComponent, oldEventComponent);
