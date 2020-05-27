6a374d51df14a36d0097f474b6866af2f1a29f4d
pepsiftd <pepsiftd@gmail.com>
2020-05-27 23:54:14 +0300

    Добавляет словарь имен фильтров

--- src/controllers/filters.js
+++ src/controllers/filters.js
@@ -1,6 +1,12 @@
 import FiltersComponent from '@/components/trip-filters';
 import {render, RenderPosition} from '@/utils/render';
 
+const FilterMode = {
+  EVERYTHING: `everything`,
+  FUTURE: `future`,
+  PAST: `past`,
+};
+
 export default class FiltersController {
   constructor(container, eventsModel) {
     this._container = container;
